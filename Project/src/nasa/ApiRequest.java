2d334d248417015f5b7d1874875841cbeeb3f51d
MorrisLaw <jmorris12@stac.edu>
2017-06-27 22:17:06 -0400

    Coded get api key method in API request class.

--- Project/src/nasa/ApiRequest.java
+++ Project/src/nasa/ApiRequest.java
@@ -1,5 +1,32 @@
 package nasa;
 
-public class ApiRequest {
+import org.apache.http.HttpResponse;
 
+public class ApiRequest {
+	/**
+	 * Gateway to NASA open API service(s).
+	 */
+	public static final String apiKey = System.getenv("NASA_API_KEY");
+	
+	/**
+	 * Returns the HTTP response in the form of JSON.
+	 * 
+	 * @param url
+	 * @return The HTTP response for the HTTP GET request.
+	 */
+	private HttpResponse ApiRequest(String url) {
+		try {
+//			HttpGet httpGet = new HttpGet("http://")
+		} finally {
+			
+		}
+	}
+	/**
+	 * 
+	 * @return The NASA API key set in the environment variables.
+	 */
+	private String GetApiKey() {
+		return apiKey;
+	}
+	
 }
