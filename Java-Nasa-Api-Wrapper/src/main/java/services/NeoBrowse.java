7270869b214526db29aaa8bfaecc1cc73bdec6d8
MorrisLaw <jmorris12@stac.edu>
2017-07-05 22:20:17 -0400

    Added page and link methods.

--- Java-Nasa-Api-Wrapper/src/main/java/services/NeoBrowse.java
+++ Java-Nasa-Api-Wrapper/src/main/java/services/NeoBrowse.java
@@ -31,7 +31,68 @@ public class NeoBrowse {
 		return json;
 	}
 	
+	public static JSONObject links() {
+		final JSONObject links = (JSONObject) neoBrowseJsonObj.get("link");
+		return links;
+	}
+	
+	/**
+	 * 
+	 * @return the next link of neo browse.
+	 */
+	public static String linkNext() {
+		return links().get("next").toString();
+	}
+	
+	/**
+	 * 
+	 * @return the current link of neo browse.
+	 */
+	public static String linkSelf() {
+		return links().get("self").toString();
+	}
+	
+	/**
+	 * 
+	 * @return page json object.
+	 */
+	public static JSONObject page() {
+		final JSONObject page = (JSONObject) neoBrowseJsonObj.get("page");
+		return page;
+	}
+	/**
+	 * 
+	 * @return the pages current size, number of neo json objects on this page.
+	 */
+	public static int pageSize() {
+		return Integer.parseInt(page().get("size").toString());
+	}
+	
+	/**
+	 * 
+	 * @return the total number of neo elements.
+	 */
+	public static int totalElements() {
+		return Integer.parseInt(page().get("total_elements").toString());
+	}
+	
+	/**
+	 * 
+	 * @return the total number of pages.
+	 */
+	public static int totalPages() {
+		return Integer.parseInt(page().get("total_pages").toString());
+	}
+	
+	/**
+	 * 
+	 * @return the current page number.
+	 */
+	public static int pageNumber() {
+		return Integer.parseInt(page().get("number").toString());
+	}
+	
 	public static void main(String[] args) {
-		System.out.println(neoBrowseJsonObj);
+		System.out.println();
 	}
 }
